d2fd1d15be0e8f04ac7843d3ca43d3d4c6a93f64
3493215187 <3493215187@qq.com>
2019-11-05 16:10:46 +0800

    项目的建立，里面进行代码的修改与添加

---
+++ myobject/src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+	<idv>
+		<h1>这是首页</h1>
+	</idv>
+</template>
+
+<script>
+	export default{
+		name:"Home"
+	}
+</script>
+
+<style>
+</style>
---
+++ myobject/src/main.js
@@ -0,0 +1,26 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from './vuex'
+import axios from 'axios'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+//配置请求信息
+Vue.prototype.$axios=axios;
+
+//使用element-ui组件，完整引入
+Vue.use(ElementUI);
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
